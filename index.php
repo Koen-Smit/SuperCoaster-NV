9d41c56691a279768e443fb9b674f21c0c5d30d0
Junbo-chen <junbochen@hotmail.com>
2022-03-28 10:04:50 +0200

    font veranderd

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
     require_once("header.php");
   ?>
   <!-- Add your site or application content here -->
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat&family=Dela+Gothic+One&family=Orbitron&display=swap');
+  </style>
   <img src="img/rollercoaster.webp" alt="">
+  <h1>Super Coaster</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae qui nihil assumenda incidunt maxime 
     ipsum sed alias modi libero repellat, aliquam veritatis ut porro beatae sunt, sequi deserunt aperiam, dolores dolor 
     cupiditate hic eius dignissimos. Molestiae consequuntur aspernatur error odit maiores laborum, excepturi repellat culpa porro, 
