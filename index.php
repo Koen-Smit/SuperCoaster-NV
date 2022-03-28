e1bba2be7b0f1d90392ffe7fd542cd4a199713c2
Junbo-chen <junbochen@hotmail.com>
2022-03-28 09:30:56 +0200

    index.php

--- index.php
+++ index.php
@@ -23,9 +23,16 @@
 </head>
 
 <body>
-
+  <?php
+    require_once(header.php);
+  ?>
   <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae qui nihil assumenda incidunt maxime 
+    ipsum sed alias modi libero repellat, aliquam veritatis ut porro beatae sunt, sequi deserunt aperiam, dolores dolor 
+    cupiditate hic eius dignissimos. Molestiae consequuntur aspernatur error odit maiores laborum, excepturi repellat culpa porro, 
+    natus corporis cum, earum sed. Quidem eum accusantium veritatis qui atque illum consequatur itaque adipisci saepe provident, libero 
+    nisi placeat ipsum blanditiis possimus explicabo at enim beatae labore sit facere! Quis aperiam atque provident excepturi minima corporis 
+    tempore harum voluptates, laboriosam sed sequi, quidem magnam tempora quo nesciunt adipisci doloremque dicta officia cum. Omnis.</p>
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
@@ -36,6 +43,9 @@
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async></script>
+  <?php
+    require_once(footer.php);
+  ?>
 </body>
 
 </html>
