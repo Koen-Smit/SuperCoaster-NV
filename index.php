7230942debe5b5da5f499c3f791d453e58eba6e0
Junbo-chen <junbochen@hotmail.com>
2022-03-30 12:48:44 +0200

    css

--- index.php
+++ index.php
@@ -6,21 +6,21 @@
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Caveat&family=Dela+Gothic+One&family=Orbitron&display=swap');
   </style>
-  <div class="class1">
-    <img src="img/rollercoaster.webp" alt="">
-    <div class="class2">
-      <h1>Super Coaster</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae qui nihil assumenda incidunt maxime 
-      ipsum sed alias modi libero repellat, aliquam veritatis ut porro beatae sunt, sequi deserunt aperiam, dolores dolor 
-      cupiditate hic eius dignissimos. Molestiae consequuntur aspernatur error odit maiores laborum, excepturi repellat culpa porro, 
-      natus corporis cum, earum sed. Quidem eum accusantium veritatis qui atque illum consequatur itaque adipisci saepe provident, libero 
-      nisi placeat ipsum blanditiis possimus explicabo at enim beatae labore sit facere! Quis aperiam atque provident excepturi minima corporis 
-      tempore harum voluptates, laboriosam sed sequi, quidem magnam tempora quo nesciunt adipisci doloremque dicta officia cum. Omnis.</p>
-    </div>
-  </div>    
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
+    <div class="class1">
+      <img src="img/rollercoaster.webp" alt="">
+      <div class="class2">
+        <h1>Super Coaster</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae qui nihil assumenda incidunt maxime 
+        ipsum sed alias modi libero repellat, aliquam veritatis ut porro beatae sunt, sequi deserunt aperiam, dolores dolor 
+        cupiditate hic eius dignissimos. Molestiae consequuntur aspernatur error odit maiores laborum, excepturi repellat culpa porro, 
+        natus corporis cum, earum sed. Quidem eum accusantium veritatis qui atque illum consequatur itaque adipisci saepe provident, libero 
+        nisi placeat ipsum blanditiis possimus explicabo at enim beatae labore sit facere! Quis aperiam atque provident excepturi minima corporis 
+        tempore harum voluptates, laboriosam sed sequi, quidem magnam tempora quo nesciunt adipisci doloremque dicta officia cum. Omnis.</p>
+      </div>
+    </div>     
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>  
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
   <script>
