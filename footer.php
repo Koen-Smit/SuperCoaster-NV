97aa4ae22828886e3ac2b37ca3b7d09b229d0ee8
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-03-30 11:29:42 +0200

    added footer
    -added social media links in footer
    -added copyright to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 <footer>
-    
+    <div class=""> 
+        <a href="">Instagram</a>
+        <a href="">Facebook</a>
+        <a href="">Linkedin</a>
+    </div>
+    <p>© by owned by SuperCoaster 2021</p>
 </footer>
