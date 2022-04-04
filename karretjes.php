eba44de8d3ae03cf4763dc070bdf09070dac5531
Junbo-chen <junbochen@hotmail.com>
2022-04-04 13:59:01 +0200

    layout van karretjes pagina toegevoegt

---
+++ karretjes.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("header.php");
+?>
+<div class="wrapper">
+    <div class="class1">
+        <img src="img/220920katloop.jpg" alt="">
+        <div class="div2">
+            <p>name: rollercoaster</p>
+            <p>park: ?</p>
+            <p>location: ?</p>
+            <p>status: dicht</p>
+            <p>snelheid: 500km/u</p>
+            <p>hoogte: 400m</p>
+            <p>gemaakt door: Supercoaster-NV</p>
+        </div>    
+    </div>
+</div>   
+<?php
+    require_once("footer.php");
+?>     
