35f159f1325499f8ada4f4e4456ce734d7230849
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-04-06 11:45:49 +0200

    header toegevoegd aan contructies

---
+++ constructies.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once("php/head.php");
+  ?>
+  <title>Supercoaster-NV</title>
+</head>
+
+<body>
+  
+  <?php
+    require_once("php/header.php");
+  ?>
+
+  <div class="wrapper">
