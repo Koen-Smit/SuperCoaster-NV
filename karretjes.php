0f974f17ae690a6a30c6551a0249de77211b2a80
Junbo-chen <junbochen@hotmail.com>
2022-04-06 12:01:05 +0200

    plaatjes toegevoegt aan karretjes pagina

--- karretjes.php
+++ karretjes.php
@@ -1,19 +1,49 @@
 <?php
-    require_once("header.php");
+    require_once("php/header.php");
+?>
+<?php
+    require_once("php/head.php");
 ?>
 <div class="wrapper">
-    <div class="class1">
-        <img src="img/plaatje7.jpg" alt="">
-        <div class="div2">
-        </div>    
-    </div>
-    <div class="class1">
-        <img src="img/plaatje8.jpg" alt="">
-    </div>
-    <div class="class1">    
-        <img src="img/plaatje9.jpg" alt="">
-    </div>    
-</div>   
+    <p id="info"></p>   
+    <p>
+        <img src="img/plaatje7.jpg" id="img">
+    </p> 
+    <p>
+        <button value="vorige" style="width: auto; height: auto;">vorige</button>
+        <button value="next">next</button>
+    </p>  
+</div> 
+<script>
+    window.onload = function(){
+        var info = document.getElementById("info");
+        var img = document.getElementById("img");
+        var path = ["img/plaatje8.jpg","img/plaatje9.jpg","img/plaatje7.jpg"];
+
+        var index = 0;
+
+        var bt = document.getElementsByTagName("button");
+
+
+        bt[0].onclick = function () {
+          index--;
+          if(index < 0){
+            index = path.length - 1;
+          }
+
+          img.src = path[index];
+        };
+
+        bt[1].onclick = function () {
+          index++;
+          if(index > path.length - 1){
+            index = 0;
+          }
+          img.src = path[index];
+        };
+      };
+
+</script>  
 <?php
-    require_once("footer.php");
+    require_once("php/footer.php");
 ?>     
