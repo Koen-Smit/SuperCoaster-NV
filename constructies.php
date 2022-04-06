ef6b1da8d9466783ecb452089eb274b33e1f1446
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-04-06 12:13:22 +0200

    Divs toegevoegd aan constructies

--- constructies.php
+++ constructies.php
@@ -10,19 +10,23 @@
     require_once("php/header.php");
   ?>
 
-  <div class="wrapper">
-
+  <div class="constructiesH1">
         <h1> De constructie
             Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. </h1>
+<br>
+    </div>
+   <div class="constructiesH2">     
+       <h2> Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. </h2>
+        <br>
+       <h2> Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. </h2>
+        <br>
+       <h2> We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. </h2>
+        <br>
+       <h2> Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. </h2>
+        <br>
+       <h2> Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </h2>
+</div>
 
-        <h2> Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. </h2>
-        <h2> Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. </h2>
-        <h2> We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. </h2>
-        <h2> Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. </h2>
-        <h2> Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </h2>
-
-
- 
 <?php
     require_once("php/footer.php");
   ?>
