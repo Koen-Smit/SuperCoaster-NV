5b7fbd5ce4252db8c3d8d6029f2e53508d901f99
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-04-11 14:30:12 +0200

    finished contact page

--- contact.php
+++ contact.php
@@ -8,14 +8,19 @@
     ?>
     <main>
         <div class="main">
-            <form action="backend/ContactController.php">
-                <input id="name" class="form-input" type="text" placeholder="Your Name*">
-                <input id="email" class="form-input" type="email" placeholder="Your Email*">
-                <input id="company-name" class="form-input" type="text" placeholder="Company Name*" required>
-                <textarea  minlength="20" id="message" cols="30" rows="7" placeholder="Your Message*" required></textarea>
+        <form class="Form" action="backend/ContactController.php">
+                <h2>Contact</h2>
+                <div class="Form_items">
+                    <input id="name" class="form_item" type="text" placeholder="Naam">
+                    <input id="email" class="form_item" type="email" placeholder="Email">
+                </div>
+                <input id="company-name" class="form_item" type="text" placeholder="Title" required>
+                <textarea class="form_item" minlength="20" id="message" cols="30" rows="7" placeholder="Uw bericht" required></textarea>
             </form>
         </div>
     </main>
-
+    <?php
+    require_once("php/footer.php");
+    ?>
 </body>
 </html>
