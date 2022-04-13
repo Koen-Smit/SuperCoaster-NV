6eb72a71c9df0e70d346783e37b2de9ce6762ec6
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-04-13 11:05:31 +0200

    constructies afgemaakt

--- constructies.php
+++ constructies.php
@@ -1,33 +1,99 @@
-<?php 
-    require_once("php/head.php");
-  ?>
-  <title>Supercoaster-NV</title>
-</head>
-
-<body>
-  
-  <?php
+<?php
     require_once("php/header.php");
-  ?>
+?>
+<?php
+    require_once("php/head.php");
+?>
+<script>
+    const docStyle = document.documentElement.style
+const aElem = document.querySelector('a')
+const boundingClientRect = aElem.getBoundingClientRect()
 
-  <div class="constructiesH1">
-        <h1> De constructie
-            Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. </h1>
-<br>
-    </div>
-   <div class="constructiesH2">     
-       <h2> Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. </h2>
-        <br>
-       <h2> Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. </h2>
-        <br>
-       <h2> We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. </h2>
-        <br>
-       <h2> Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. </h2>
-        <br>
-       <h2> Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </h2>
-</div>
+aElem.onmousemove = function(e) {
 
+    const x = e.clientX - boundingClientRect.left
+    const y = e.clientY - boundingClientRect.top
+    
+    const xc = boundingClientRect.width/2
+    const yc = boundingClientRect.height/2
+    
+    const dx = x - xc
+    const dy = y - yc
+    
+    docStyle.setProperty('--rx', `${ dy/-1 }deg`)
+    docStyle.setProperty('--ry', `${ dx/10 }deg`)
+    
+}
+
+aElem.onmouseleave = function(e) {
+    
+    docStyle.setProperty('--ty', '0')
+    docStyle.setProperty('--rx', '0')
+    docStyle.setProperty('--ry', '0')
+    
+}
+
+aElem.onmousedown = function(e) {
+    
+    docStyle.setProperty('--tz', '-25px')
+    
+}
+
+document.body.onmouseup = function(e) {
+    
+    docStyle.setProperty('--tz', '-12px')
+    
+}
+</script>
+<div class="wrapper">
+    <h1 id="karretje" style="margin-top: 30px;">Over Constructie</h1>
+    <div class="class1">
+        <div class="class3">
+            <p id="info"></p>   
+            <p>
+                <img src="img/plaatje10.jpg" id="img">
+            </p>
+
+            <div class="div4">
+                <button class="bttn-minimal bttn-md bttn-primary" value="vorige">vorige</button>
+                <button class="bttn-minimal bttn-md bttn-primary" value="next">next</button>
+            </div>    
+        </div>    
+        <div class="class2">
+            <p>Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</p>
+        </div>    
+    </div>    
+</div> 
+
+<script>
+    window.onload = function(){
+        var info = document.getElementById("info");
+        var img = document.getElementById("img");
+        var path = ["img/plaatje10.jpg","img/plaatje11.jpg","img/plaatje12.jpg", "img/plaatje13.jpg"];
+
+        var index = 0;
+
+        var bt = document.getElementsByTagName("button");
+
+
+        bt[0].onclick = function () {
+          index--;
+          if(index < 0){
+            index = path.length - 1;
+          }
+
+          img.src = path[index];
+        };
+
+        bt[1].onclick = function () {
+          index++;
+          if(index > path.length - 1){
+            index = 0;
+          }
+          img.src = path[index];
+        };
+      };
+</script>  
 <?php
     require_once("php/footer.php");
-  ?>
-  </body> 
+?>     
