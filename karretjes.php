6251aae90eb6aaf9223fbf4eeb7b8563f615e84b
KerrieMeister96 <102681513+KerrieMeister96@users.noreply.github.com>
2022-04-17 00:47:56 +0200

    cleaned up old code and added new lines and changed order of them

--- karretjes.php
+++ karretjes.php
@@ -1,33 +1,25 @@
   <?php require_once("php/head.php");?>
   <title>Supercoaster-NV</title>
 </head>
-
 <body>
-  
-  <?php
-    require_once("php/header.php");
-  ?>
-
-<div class="wrapper">
-    <h1 id="karretje" style="margin-top: 30px;">Over Karretjes</h1>
-    <div class="class1">
-        <div class="class3">
-            <p id="info"></p>   
-            <p>
-                <img src="img/plaatje7.jpg" id="img">
-            </p>
-
-            <div class="div4">
+  <?php require_once("php/header.php");?>
+  <div class="wrapper">
+    <main>
+      <div class="supercoaster">
+        <h1 class="h1karretjes">Over Karretjes</h1>
+      </div>
+      
+      <div class="homepageinfo">
+        <div class="flexColumn">
+          <img src="img/plaatje7.jpg" id="img">
+            <div class="imageButtons">
                 <button class="bttn-minimal bttn-md bttn-primary" value="vorige">vorige</button>
-                <button class="bttn-minimal bttn-md bttn-primary" value="next">next</button>
-            </div>    
-        </div>    
-        <div class="class2">
-            <p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren. Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia.</p>
-        </div>    
-    </div>    
-</div> 
-
+                <button class="bttn-minimal bttn-md bttn-primary" value="next">volgende</button>
+            </div>
+        </div>  
+        <div class="karretjes">    
+          <p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden.</p>
+          <p>Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren. Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia.</p>
         </div>
       </div>
     </main>
