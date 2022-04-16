66a35366322483af1ea2e00747ac0b6dfb785fa4
KerrieMeister96 <102681513+KerrieMeister96@users.noreply.github.com>
2022-04-17 00:53:39 +0200

    added divs, images and p tags

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
   <?php require_once("php/header.php");?>
 
   <div class="wrapper">
+    <main>
+      <div class="supercoaster">
+        <h1 class="h1Home">Super Coaster</h1>
+      </div>
+      <div class="homespan">
+        <div class="homespan1">
+          <p> Onze nieuwe achtbaantechniek heet MagnaDrive. Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe,</p>
+          <img src="img/plaatje3.jpg"/>
+        </div>
+        <div class="homespan1">
+          <img src="img/plaatje4.jpg"/>
+          <p> enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen. Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie.</p>
+        </div>
+        <div class="homespan1">
+          <p> Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
+          <img src="img/plaatje6.jpg"/>
+        </div>
+      </div>
 
       
     </main>
