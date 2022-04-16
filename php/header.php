0f65e7bb513298f858079e80b219a38ddede2907
KerrieMeister96 <102681513+KerrieMeister96@users.noreply.github.com>
2022-04-17 00:36:05 +0200

    removed old burger menu and added simple nav

--- php/header.php
+++ php/header.php
@@ -1,22 +1,11 @@
 <header>
-  <a href="index.php"><img class="headerImage" src="img/logo.png" alt="Logo"></a>
-
-  <section class="p-menu1">
-
-    <nav id="navbar" class="navigation" role="navigation">
-      <input id="toggle1" type="checkbox" />
-      <label class="hamburger1" for="toggle1">
-        <div class="top"></div>
-        <div class="middle"></div>
-        <div class="bottom"></div>
-      </label> 
-      <nav class="menu1">
-        <a class="link1" href="index.php">Home</a>
-        <a class="link1" href="constructies.php">Constructie</a>
-        <a class="link1" href="karretjes.php">Karretjes</a>
-        <a class="link1" href="contact.php">Contact</a>
+  <div class="header">
+    <a href="index.php"><img class="headerImage" src="img/logo.png" alt="Logo"></a>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="constructies.php">Constructie</a>
+      <a href="karretjes.php">Karretjes</a>
+      <a href="contact.php">Contact</a>
     </nav>
-
-    </nav>
-  </section>
+    </div>
 </header>
