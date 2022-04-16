6cab96e0d77479be9e97049ba68d2a12b039eca8
KerrieMeister96 <102681513+KerrieMeister96@users.noreply.github.com>
2022-04-17 00:48:38 +0200

    changed the form

--- contact.php
+++ contact.php
@@ -13,10 +13,16 @@
                     <input name="name"id="name" class="form_item" type="text" placeholder="Naam">
                     <input name="email"id="email" class="form_item" type="email" placeholder="Email">
                 </div>
-                <input name="title"id="title" class="form_item" type="text" placeholder="Title" required>
-                <textarea name="message"class="form_item" minlength="20" id="message" cols="30" rows="7" placeholder="Uw bericht" required></textarea>
-                <input type="submit" value="Verzend formulier">
-            </form>
+                
+                <form class="Form" action="backend/ContactController.php" method="POST">
+                    <div class="Form_items">
+                        <input name="name"id="name" class="form_item" type="text" placeholder="Naam">
+                        <input name="email"id="email" class="form_item" type="email" placeholder="Email">
+                    </div>
+                    <input name="title"id="title" class="form_item" type="text" placeholder="Titel" required>
+                    <textarea name="message"class="form_item" minlength="20" id="message" cols="30" rows="7" placeholder="Uw bericht" required></textarea>
+                    <input class="submit" type="submit" value="Verzend formulier">
+                </form>
             </div>
         </div>
     </main>
