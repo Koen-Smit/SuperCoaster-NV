013a385df03a4da1cf2ac410e917c2e9f89400ed
KerrieMeister96 <102681513+KerrieMeister96@users.noreply.github.com>
2022-04-17 00:45:23 +0200

    changed layour of code and added p tags

--- constructies.php
+++ constructies.php
@@ -2,12 +2,22 @@
     <title>Supercoaster-NV</title>
 </head>
 <body>
+    <?php require_once("php/header.php");?>
+    <div class="wrapper">
+        <main>
+            <div class="supercoaster">
+                <h1 class="h1Home">Over Constructie</h1>
+            </div>
+            <div class="constructieinfo">
+                <p>Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen.</p>
                 <marquee behavior=scroll direction="right" scrollamount="12">
                     <img src="img/plaatje10.jpg" alt="constructieimg">
                     <img src="img/plaatje11.jpg" alt="constructieimg">
                     <img src="img/plaatje12.jpg" alt="constructieimg">
                     <img src="img/plaatje13.jpg" alt="constructieimg">
                 </marquee>
+                <p>Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</p>
+            </div>
 
 aElem.onmousemove = function(e) {
 
